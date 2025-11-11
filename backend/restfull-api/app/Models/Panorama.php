964114dbac8243d2ca91d4d0a1841ee6b91cf00d
David-Schoots <davidschoots@live.nl>
2025-11-11 10:28:18 +0100

    added model, controller, db

---
+++ backend/restfull-api/app/Models/Panorama.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Panorama extends Model
+{
+    //
+    protected $fillable = [
+    'page',
+    'catalog_number',
+    'description',
+    'img',
+];
+
+}
