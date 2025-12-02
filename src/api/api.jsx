d2b2e2bb1fbb8c85d4304b10f27ffe2495db6dd8
David-Schoots <davidschoots@live.nl>
2025-12-02 14:41:49 +0100

    added test voor cms

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -3,12 +3,13 @@ import axios from "axios";
 const API_URL = "http://localhost:8000/api";
 
 const getAuthHeaders = () => {
-  const token = localStorage.getItem("api_token");
+  const token = localStorage.getItem("auth_token");
   if (!token) {
     console.warn("Geen API token gevonden in localStorage");
   }
   return {
     "Content-Type": "application/json",
+    Accept: "application/json",
     Authorization: `Bearer ${token}`,
   };
 };
